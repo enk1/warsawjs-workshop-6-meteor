9ad5ce2e1f745505d35d9f00b458715aa022d8ab
enkidu <grzywaczewski.jakub@gmail.com>
2017-05-21 13:43:50 +0200

    links to rooms

--- whiteboard/imports/startup/client/routes.js
+++ whiteboard/imports/startup/client/routes.js
@@ -4,6 +4,7 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/room/room.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
@@ -12,6 +13,13 @@ FlowRouter.route('/', {
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
   },
+});
+
+FlowRouter.route('/rooms/:id', {
+  name: 'App.room',
+  action(){
+    BlazeLayout.render('App_body', { main: 'App_room' });
+  }
 });
 
 FlowRouter.notFound = {
