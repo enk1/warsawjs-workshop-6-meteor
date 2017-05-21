63c81e6e34bddef5f023ce83559e08a2f660da28
enkidu <grzywaczewski.jakub@gmail.com>
2017-05-21 14:08:51 +0200

    room title and id in template

--- whiteboard/imports/api/rooms/server/publications.js
+++ whiteboard/imports/api/rooms/server/publications.js
@@ -6,3 +6,6 @@ import { Rooms } from '../rooms.js';
 Meteor.publish('rooms.all', function () {
   return Rooms.find();
 });
+Meteor.publish('rooms.show', function(roomId){
+    return Rooms.find({_id: roomId});
+});
--- whiteboard/imports/ui/pages/room/room.js
+++ whiteboard/imports/ui/pages/room/room.js
@@ -1,1 +1,14 @@
+import {Meteor} from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Rooms } from '/imports/api/rooms/rooms.js';
 import './room.html';
+
+Template.App_room.onCreated(function(){
+    Meteor.subscribe('rooms.show', FlowRouter.getParam('id'));
+});
+
+Template.App_room.helpers({
+    room(){
+        return Rooms.findOne(FlowRouter.getParam('id'));
+    }
+});
